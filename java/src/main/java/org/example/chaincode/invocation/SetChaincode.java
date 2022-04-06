a3805c45cb8c8499393462990f4095273d043cb0
Chsui <choco.nina.mc@gmail.com>
2022-04-06 15:30:42 +0900

    create SetChaincode.java

---
+++ java/src/main/java/org/example/chaincode/invocation/SetChaincode.java
@@ -0,0 +1,70 @@
+package org.example.chaincode.invocation;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.example.client.CAClient;
+import org.example.client.ChannelClient;
+import org.example.client.FabricClient;
+import org.example.config.Config;
+import org.example.user.UserContext;
+import org.example.util.Util;
+import org.hyperledger.fabric.sdk.*;
+import org.hyperledger.fabric.sdk.ChaincodeResponse.Status;
+
+public class SetChaincode {
+    private static final byte[] EXPECTED_EVENT_DATA = "!".getBytes(UTF_8);
+    private static final String EXPECTED_EVENT_NAME = "event";
+
+    public static void main(String args[]) {
+        try {
+            Util.cleanUp();
+            String caUrl = Config.CA_ORG1_URL;
+            CAClient caClient = new CAClient(caUrl, null);
+            UserContext adminUserContext = new UserContext();
+            adminUserContext.setName(Config.ADMIN);
+            adminUserContext.setAffiliation(Config.ORG1);
+            adminUserContext.setMspId(Config.ORG1_MSP);
+            caClient.setAdminUserContext(adminUserContext);
+            adminUserContext = caClient.enrollAdminUser(Config.ADMIN, Config.ADMIN_PASSWORD);
+
+            FabricClient fabClient = new FabricClient(adminUserContext);
+
+            ChannelClient channelClient = fabClient.createChannelClient(Config.CHANNEL_NAME);
+            Channel channel = channelClient.getChannel();
+            Peer peer = fabClient.getInstance().newPeer(Config.ORG1_PEER_0, Config.ORG1_PEER_0_URL);
+            EventHub eventHub = fabClient.getInstance().newEventHub("eventhub01", "grpc://localhost:7053");
+            Orderer orderer = fabClient.getInstance().newOrderer(Config.ORDERER_NAME, Config.ORDERER_URL);
+            channel.addPeer(peer);
+            channel.addEventHub(eventHub);
+            channel.addOrderer(orderer);
+            channel.initialize();
+
+            TransactionProposalRequest request = fabClient.getInstance().newTransactionProposalRequest();
+            ChaincodeID ccid = ChaincodeID.newBuilder().setName(Config.CHAINCODE_1_NAME).build();
+            request.setChaincodeID(ccid);
+            request.setFcn("set");
+            request.setArgs(args);
+            request.setProposalWaitTime(1000);
+
+            Map<String, byte[]> tm2 = new HashMap<>();
+            tm2.put("HyperLedgerFabric", "TransactionProposalRequest:JavaSDK".getBytes(UTF_8));
+            tm2.put("method", "TransactionProposalRequest".getBytes(UTF_8));
+            tm2.put("result", ":)".getBytes(UTF_8));
+            tm2.put(EXPECTED_EVENT_NAME, EXPECTED_EVENT_DATA);
+            request.setTransientMap(tm2);
+            Collection<ProposalResponse> responses = channelClient.sendTransactionProposal(request);
+            for (ProposalResponse res: responses) {
+                Status status = res.getStatus();
+                Logger.getLogger(InvokeChaincode.class.getName()).log(Level.INFO,"Invoked set on "+Config.CHAINCODE_1_NAME + ". Status - " + status);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
