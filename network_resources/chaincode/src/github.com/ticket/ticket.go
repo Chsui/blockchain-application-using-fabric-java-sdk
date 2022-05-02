7bf2491871cdcbf30d99a36ee7a4e8e0aae33934
Chsui <choco.nina.mc@gmail.com>
2022-05-03 02:18:45 +0900

    update

--- network_resources/chaincode/src/github.com/ticket/ticket.go
+++ network_resources/chaincode/src/github.com/ticket/ticket.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/peer"
 	sc "github.com/hyperledger/fabric/protos/peer"
 	"strconv"
 )
@@ -45,6 +46,8 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 		return s.changeOwner(APIstub, args)
 	} else if function == "deleteTicket" {
 		return s.deleteTicket(APIstub, args)
+	} else if function == "getTransactionByID" {
+		return s.getTransactionByID(APIstub, args)
 	}
 	return shim.Error("Invalid Function name.")
 }
@@ -140,6 +143,18 @@ func (s *SmartContract) deleteTicket(APIstub shim.ChaincodeStubInterface, args [
 		return shim.Error(err.Error())
 	}
 	return shim.Success(nil)
+}
+
+func (s *SmartContract) getTransactionByID(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	if len(args) != 1 {
+		return shim.Error("Error Incorrect arguments")
+	}
+	ledger := peer.GetLedger(APIstub.GetChannelID())
+	tx, err := ledger.GetTransactionByID(args[0])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success([]byte(tx.String()))
 }
 
 func main() {
